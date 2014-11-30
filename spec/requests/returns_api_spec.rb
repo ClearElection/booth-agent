19e0f6fe11df6b452b8554063ab6d12d9a381c7d
ronen barzel <ronen@barzel.org>
2014-11-30 07:21:35 +0000

    use shared examples for api validates election

--- spec/requests/returns_api_spec.rb
+++ spec/requests/returns_api_spec.rb
@@ -1,42 +1,9 @@
 require "rails_helper"
 
 describe "Returns API" do
-  Given(:validElection) { ClearElection::Factory.election(booth: my_agent_uri) }
-  Given(:validElectionUri) { stub_election_uri(election: validElection) }
 
-  Given(:otherAgentElectionUri) { stub_election_uri() }
-  Given(:invalidElectionUri) { stub_election_uri(valid: false) }
-
-  When { get "/returns", election: election_uri }
-
-  describe "with valid election" do
-    Given(:election_uri) { validElectionUri }
-
-    describe "if polls are closed" do
-      Given{ Timecop.travel(validElection.pollsClose + 1.day) }
-      Then { expect(response).to have_http_status 200 }
-    end
-
-    describe "if polls are not closed" do
-      Given{ Timecop.travel(validElection.pollsClose - 1.day) }
-      Then { expect(response).to have_http_status 403 }
-      Then { expect(response_error_message).to match /closed/i }
-    end
-
+  it_behaves_like "api validates election URI", agent: :booth, state: :closed do
+    let(:apicall) { -> election_uri { get "/returns", election: election_uri} }
   end
 
-  describe "with invalid election" do
-
-    describe "other booth agent" do
-      Given(:election_uri) { otherAgentElectionUri }
-      Then { expect(response).to have_http_status 422 }
-      Then { expect(response_json["error"]).to match /booth agent/i }
-    end
-
-    describe "invalid uri" do
-      Given(:election_uri) { invalidElectionUri }
-      Then { expect(response).to have_http_status 422 }
-      Then { expect(response_json["error"]).to match /uri/i }
-    end
-  end
 end
--- spec/requests/session_api_spec.rb
+++ spec/requests/session_api_spec.rb
@@ -1,18 +1,17 @@
 require "rails_helper"
 
 describe "Session API" do
-  Given(:validElection) { ClearElection::Factory.election(booth: my_agent_uri) }
-  Given(:validElectionUri) { stub_election_uri(election: validElection) }
-  Given(:otherAgentElectionUri) { stub_election_uri() }
-  Given(:invalidElectionUri) { stub_election_uri(valid: false) }
 
-  Given(:validAccessToken) { stub_election_access_token(election_uri: validElectionUri) }
-  Given(:invalidAccessToken) { stub_election_access_token(election_uri: validElectionUri, valid: false) }
+  it_behaves_like "api validates election URI", agent: :booth, state: :open do
+    let(:apicall) { -> election_uri { post "/session", election: election_uri, accessToken: "anything" } }
+  end
 
-  When { post "/session", election: election_uri, accessToken: accessToken }
+  describe "with valid election URI" do
+    Given(:election_uri) { stub_election_uri(booth: my_agent_uri) }
+    Given(:validAccessToken) { stub_election_access_token(election_uri: election_uri) }
+    Given(:invalidAccessToken) { stub_election_access_token(election_uri: election_uri, valid: false) }
 
-  describe "with valid election" do
-    Given(:election_uri) { validElectionUri }
+    When { post "/session", election: election_uri, accessToken: accessToken }
 
     describe "with valid access token" do
       Given(:accessToken) { validAccessToken }
@@ -26,36 +25,7 @@ describe "Session API" do
       Then { expect(response_json["error"]).to match /token/i }
     end
 
-    describe "if polls haven't opened yet" do
-      Given(:accessToken) { "anything" }
-      Given{ Timecop.travel(validElection.pollsOpen - 1.day) }
-      Then { expect(response).to have_http_status 403 }
-      Then { expect(response_json["error"]).to match /open/i }
-    end
-
-    describe "if polls are closed" do
-      Given(:accessToken) { "anything" }
-      Given{ Timecop.travel(validElection.pollsClose + 1.day) }
-      Then { expect(response).to have_http_status 403 }
-      Then { expect(response_json["error"]).to match /open/i }
-    end
-
   end
 
-  describe "with invalid election" do
-    Given(:accessToken) { "anything" }
-
-    describe "with other booth agent" do
-      Given(:election_uri) { otherAgentElectionUri }
-      Then { expect(response).to have_http_status 422 }
-      Then { expect(response_json["error"]).to match /booth agent/i }
-    end
-
-    describe "invalid uri" do
-      Given(:election_uri) { invalidElectionUri }
-      Then { expect(response).to have_http_status 422 }
-      Then { expect(response_json["error"]).to match /uri/i }
-    end
-  end
 
 end
